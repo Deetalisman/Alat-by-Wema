978149f16e0c25d1662c860145fcc4bb68035b30
Deetalisman <ishmaeldotun@gmail.com>
2024-07-26 22:25:45 +1000

    page1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-let login = document.getElementById("mainlogin");
+let home1 = document.getElementById("home11");
+let home2 = document.getElementById("home2");
+
+function showhome() {
+  home1.style.display = "none";
+  home2.classList.remove("hide");
+}
+
+setTimeout(showhome, 2000);

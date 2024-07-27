af3db47f8ebc2aa00ccadc776973d511a10b6856
Deetalisman <ishmaeldotun@gmail.com>
2024-07-27 10:52:19 +1000

    personalaccount

--- index3.js
+++ index3.js
@@ -5,26 +5,69 @@ let paysection = document.getElementById("payment-section");
 let loansection = document.getElementById("loan-section");
 let cardsection = document.getElementById("card-section");
 let goalsection = document.getElementById("goal-section");
+let viewsection = document.getElementById("view-sect");
 let aside = document.getElementById("aside");
 const open = document.getElementById("open");
 let back = document.getElementById("Back");
+let backs = document.getElementById("Backs");
+let backss = document.getElementById("Backss");
 let feet = document.getElementById("feets");
+let view = document.getElementById("view");
 let payicon = document.getElementById("pay-icon");
 let homeicon = document.getElementById("home-icon");
 let loanicon = document.getElementById("loan-icon");
 let cardicon = document.getElementById("card-icon");
 let goalicon = document.getElementById("goal-icon");
+let peracc = document.getElementById("peracc");
+let viewopt1 = document.getElementById("viewopt1");
+let viewopt2 = document.getElementById("viewopt2");
+let back0 = document.getElementById("back0");
+let back1 = document.getElementById("back1");
 
+//To open the side bar
 open.addEventListener("click", () => {
   aside.classList.add("show");
   section.classList.add("hide");
   feet.classList.add("hide");
 });
+
+//To close the side bar
 back.addEventListener("click", () => {
   aside.classList.add("hide");
   section.classList.add("show");
 });
 
+//To open the view all accounts
+view.addEventListener("click", () => {
+  viewsection.classList.remove("hide");
+  section.classList.add("hide");
+  feet.classList.add("hide");
+});
+
+//To close the view all accounts
+backs.addEventListener("click", () => {
+  viewsection.classList.add("hide");
+  section.classList.remove("hide");
+  feet.classList.remove("hide");
+});
+
+//To open personal account
+peracc.addEventListener("click", () => {
+  viewopt1.classList.add("hide");
+  viewopt2.classList.remove("hide");
+  back0.classList.add("hide");
+  back1.classList.remove("hide");
+});
+
+//To close personal accounts
+backss.addEventListener("click", () => {
+  viewopt2.classList.add("hide");
+  viewopt1.classList.remove("hide");
+  back1.classList.add("hide");
+  back0.classList.remove("hide");
+});
+
+//To open payment section
 payicon.addEventListener("click", () => {
   paysection.classList.remove("hide");
   section.classList.add("hide");
@@ -38,6 +81,7 @@ payicon.addEventListener("click", () => {
   goalicon.classList.remove("fpic");
 });
 
+//To open home section
 homeicon.addEventListener("click", () => {
   section.classList.remove("hide");
   paysection.classList.add("hide");
@@ -51,6 +95,7 @@ homeicon.addEventListener("click", () => {
   goalicon.classList.remove("fpic");
 });
 
+//To open Loan section
 loanicon.addEventListener("click", () => {
   section.classList.add("hide");
   loansection.classList.remove("hide");
@@ -64,6 +109,7 @@ loanicon.addEventListener("click", () => {
   goalicon.classList.remove("fpic");
 });
 
+//To open Card section
 cardicon.addEventListener("click", () => {
   section.classList.add("hide");
   loansection.classList.add("hide");
@@ -77,6 +123,7 @@ cardicon.addEventListener("click", () => {
   goalicon.classList.remove("fpic");
 });
 
+//To open Goal section
 goalicon.addEventListener("click", () => {
   section.classList.add("hide");
   loansection.classList.add("hide");
